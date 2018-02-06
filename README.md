2fdc0a9f251f6c75d0e136a9cdaf2c0bfb59bc32
Quinten Clause <quinten.clause@studioemma.eu>
2018-02-06 23:41:35 +0100

    Restructure project

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # mirrored
 
-http://tructroc.be/nl/index.html
+## webcam to html5 canvas experiment
+
+Demo 1:
+Demo 2:
+
+### About this demo
+
+Two simple experiments for redrawing data from the webcam to a html5 canvas.
+
+### Running the code on your local machine
+
+Once you have cloned this repo, you can run a local dev server with:
+
+```
+node server.js
+```
+
+And open http://localhost:4187/ in your browser.
+
+### License
+
+MIT license
--- server.js
+++ server.js
@@ -1,13 +1,55 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
 
-app.use('/',express.static(__dirname + '/'));
+http.createServer(function (request, response) {
+    console.log('request ', request.url);
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-});
+    var filePath = './pub' + request.url;
+    if (filePath == './pub/') {
+        filePath = './pub/index.html';
+    }
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
-});
+    var extname = String(path.extname(filePath)).toLowerCase();
+    var contentType = 'text/html';
+    var mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.svg': 'application/image/svg+xml'
+    };
+
+    contentType = mimeTypes[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT'){
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                response.end();
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+
+}).listen(4187);
+console.log('Server running at http://localhost:4187/');
